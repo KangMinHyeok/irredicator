545da1dd5c0712ee800e35f22d90ea5ba4ae2aaa
KangMinHyeok <saigoyo@nate.com>
2024-05-05 16:15:09 +0900

    update

--- analysis/spark_analyze_inconsistent_prefixes.py
+++ analysis/spark_analyze_inconsistent_prefixes.py
@@ -17,9 +17,89 @@ from pyspark import SparkContext, StorageLevel, SparkConf, broadcast
 
 sys.path.append('/home/mhkang/rpki-irr/irredicator/')
 from as_info.as2rel import AS2Rel
-from utils.binaryPrefixTree import make_binary_prefix_tree, get_records
-from utils.utils import write_result, ip2binary, get_date, get_dates, get_files, make_dirs
+# from utils.binaryPrefixTree import make_binary_prefix_tree, get_records
+from utils.utils import write_result, get_date, get_dates, get_files, make_dirs    
+
+def ip2binary(prefix_addr, prefix_len):
+    if("." in prefix_addr): # IPv4
+        octets = map(lambda v: int(v), prefix_addr.split("."))
+        octets = map(lambda v: format(v, "#010b")[2:], octets)
+    else: # IPv6
+        octets = map(lambda v: str(v), prefix_addr.split(":"))
+        prefix_addrs = prefix_addr.split(":")
+        for i in range( 8 - len(prefix_addrs)):
+            idx = prefix_addrs.index("")
+            prefix_addrs.insert(idx, "")
+        prefix_addrs += [""] * (8 - len(prefix_addrs)) # 8 groups, each of them has 16 bytes (= four hexadecimal digits)
+        octets = []
+        for p in prefix_addrs:
+            if( len(p) != 4): # 4 bytes
+                p = (4 - len(p)) * '0' + p
+            for bit in p:
+                b = format(int(bit, 16), "04b")
+                octets.append( b)
+    return "".join(octets)[:int(prefix_len)]
+
+def make_binary_prefix_tree(records):
+    tree = {}
+    record_set = {}
+    for record in records:
+        prefix_addr, prefix_len = record[0], record[1]
+        binary_prefix = ip2binary(prefix_addr, prefix_len)
+
+        insert(tree, record_set, binary_prefix, record)
+        
+    return tree, record_set
+
+def insert(tree, record_set, binary_prefix, record):
+
+    if binary_prefix not in record_set:
+        record_set[binary_prefix] = set()
     
+    record_set[binary_prefix].add(tuple(record))
+    insert2tree(tree, binary_prefix)
+
+def insert2tree(tree, binary_prefix):
+    if len(binary_prefix) == 0:
+        return
+    
+    subtree = tree
+    for i in range(len(binary_prefix)):
+        if binary_prefix[i] not in subtree:
+            subtree[binary_prefix[i]] = {}
+
+        subtree = subtree[binary_prefix[i]]
+
+        if i == len(binary_prefix)-1:
+            subtree['l']=True
+
+def get_covered(tree, binary_prefix):
+    covered=[]
+    subtree = tree
+    subp=""
+    for t in binary_prefix:
+        if t not in subtree:
+            break
+        subtree = subtree[t]
+        subp = subp + t
+        if 'l' in subtree:
+            covered.append(subp)
+
+    return sorted(covered, key=lambda x: len(x))
+
+def get_records(tree, record_set, binary_prefix):
+    
+    binary_prefixes = get_covered(tree, binary_prefix)
+    
+    records = []
+
+    if len(binary_prefixes) == 0:
+        return records
+
+    for binary_prefix in binary_prefixes:
+        records += record_set.get(binary_prefix, [])
+    
+    return records
 
 def parseVRP(line, ip_version='ipv4'):
     if line.startswith('#'): return []
