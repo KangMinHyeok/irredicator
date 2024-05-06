4d718c7f30ead07c2bb14046b4c11630eb694e3b
KangMinHyeok <saigoyo@nate.com>
2024-05-06 12:01:23 +0900

    update

--- crontab/routeviews.sh
+++ crontab/routeviews.sh
@@ -7,6 +7,6 @@ export SPARK_HOME=/usr/local/hadoop/spark
 export LD_LIBRARY_PATH=/usr/local/hadoop/lib/native:$LD_LIBRARY_PATH
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.4-src.zip:$PYTHONPATH
 
-(cd /net/data/routeviews/update/; for f in *; do hdfs dfs -put "/net/data/routeviews/update/$f" "/user/mhkang/routeviews/update/$f"; done)
+(cd /net/data/routeviews/update/; for f in *; do hdfs dfs -put "/net/data/routeviews/update/$f" "/user/mhkang/routeviews/update"; done)
 /home/mhkang/.local/bin/spark-submit /home/mhkang/rpki-irr/irredicator/preprocess/spark_reduce_bgp_update.py >> /home/mhkang/rpki-irr/logs/spark_reduce_bgp_update.log 2>&1
 /usr/local/hadoop/bin/hdfs dfs -put /net/data/routeviews/reduced/*.tsv /user/mhkang/routeviews/reduced >> /home/mhkang/rpki-irr/logs/put-routeviews-reduced.log 2>&1
