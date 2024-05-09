85cd61b2f6fd17384e0bb6cc9787279575e98032
KangMinHyeok <saigoyo@nate.com>
2024-05-09 12:21:17 +0900

    update

--- crontab/inconsistency.sh
+++ crontab/inconsistency.sh
@@ -8,4 +8,5 @@ export LD_LIBRARY_PATH=/usr/local/hadoop/lib/native:$LD_LIBRARY_PATH
 export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.10.4-src.zip:$PYTHONPATH
 
 /home/mhkang/.local/bin/spark-submit /home/mhkang/rpki-irr/irredicator/analysis/spark_analyze_inconsistent_prefixes.py >> /home/mhkang/rpki-irr/logs/spark_analyze_inconsistent_prefixes.log 2>&1
-# /home/mhkang/.local/bin/spark-submit /home/mhkang/rpki-irr/irredicator/analysis/spark_analyze_inconsistent_bgp_coverage.py >> /home/mhkang/rpki-irr/logs/spark_analyze_inconsistent_bgp_coverage.log 2>&1
+/home/mhkang/.local/bin/spark-submit /home/mhkang/rpki-irr/irredicator/analysis/spark_analyze_inconsistent_bgp_coverage.py >> /home/mhkang/rpki-irr/logs/spark_analyze_inconsistent_bgp_coverage.log 2>&1
+
